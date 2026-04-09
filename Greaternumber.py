8c1ecd0602d5affd5f6d7ec0167aa9d96255060c
https://github.com/alfa546 <noumansajid623@gmail.com>
2026-04-09 19:54:23 +0500

    Find greater between two number

---
+++ Greaternumber.py
@@ -0,0 +1,9 @@
+a = input("Enter a number :")
+b = input("Enter a second number : ")
+if a > b :
+    print (a,"The number is greater than",b)
+elif a<b :
+    print (b,"The number is greater than",a)
+else :
+    print ("Both are equal")
+
